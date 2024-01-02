546d990758dbcaf040b9b0c65021af7563410a6e
baeseungwon <21storeo@gmail.com>
2024-01-02 09:32:45 +0900

    msg

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,3 +1,34 @@
+resource "aws_iam_policy" "eks_ebs_policy" {
+  name = "eks-ebs-policy"
+  path = "/"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "ec2:AttachVolume",
+          "ec2:CreateSnapshot",
+          "ec2:CreateTags",
+          "ec2:CreateVolume",
+          "ec2:DeleteSnapshot",
+          "ec2:DeleteTags",
+          "ec2:DeleteVolume",
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSnapshots",
+          "ec2:DescribeTags",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeVolumesModifications",
+          "ec2:DetachVolume",
+          "ec2:ModifyVolume"
+        ]
+        Effect   = "Allow",
+        Resource = "*"
+    }]
+  })
+}
+
+
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
 
@@ -30,6 +61,36 @@ resource "aws_iam_role" "eks_node_role" {
 }
 
 
+resource "aws_iam_role" "eks_sa_role" {
+  name = "eks-sa-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::370080849531:oidc-provider/oidc.eks.ap-northeast-2.amazonaws.com/id/97EBD2A3048EF0235BBE433E6B079C1F"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "oidc.eks.ap-northeast-2.amazonaws.com/id/97EBD2A3048EF0235BBE433E6B079C1F:aud" : "sts.amazonaws.com",
+            "oidc.eks.ap-northeast-2.amazonaws.com/id/97EBD2A3048EF0235BBE433E6B079C1F:sub" : "system:serviceaccount:kube-system:ebs-csi-controller"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_ebs_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  role       = aws_iam_role.eks_sa_role.name
+}
+
+
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_cluster_role.name
@@ -56,6 +117,18 @@ resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
 
 resource "aws_iam_role_policy_attachment" "ecr-read-only" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_ebs_policy_master_attachment" {
+  policy_arn = aws_iam_policy.eks_ebs_policy.arn
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_ebs_policy_node_attachment" {
+  policy_arn = aws_iam_policy.eks_ebs_policy.arn
   role       = aws_iam_role.eks_node_role.name
 }
 
